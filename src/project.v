87c2a4652d2fae4bb05e11484fcafcfe1b2f10f0
Yoda1999 <67253699+Yoda1999@users.noreply.github.com>
2024-12-26 12:29:37 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,21 +6,25 @@
 `default_nettype none
 
 module tt_um_yoda_1999full_adder (
-    input wire a,        // First input
-    input wire b,        // Second input
-    input wire cin,      // Carry input
-    input  wire clk,
-    input  wire       ena,
-    output wire sum,     // Sum output
-    output  wire cout,    // Carry output
-    input  wire       rst_n,
-    input  wire [7:0] ui_in,
-    input wire [7:0] uio_in
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
     // Internal signals
+    wire a,b,cin,sum,cout;
     wire sum1, c1, c2;
-    wire _unused = &{clk,ena,rst_n,ui_in,1'b0};
+    wire _unused = &{clk,ena,rst_n,uio_in,1'b0};
+
+    assign a=ui_in[0];
+    assign b=ui_in[1];
+    assign c=ui_in[2];
+                   
     // Sum calculation
     xor (sum1, a, b);
     xor (sum, sum1, cin);
@@ -30,6 +34,9 @@ module tt_um_yoda_1999full_adder (
     and (c2, sum1, cin);
     or (cout, c1, c2);
 
+    assign uo_out={6'b0,cout,sum};
+    assign uio_out = 0;
+    assign uio_oe  = 0;
 endmodule
 
 
