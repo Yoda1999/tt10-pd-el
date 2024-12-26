3cdf7ca4e7cdd536f44a9651f6b0f3cdd4c1aa3f
Yoda1999 <67253699+Yoda1999@users.noreply.github.com>
2024-12-26 12:20:53 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -13,12 +13,13 @@ module tt_um_yoda_1999full_adder (
     input  wire       ena,
     output wire sum,     // Sum output
     output  wire cout,    // Carry output
-    input  wire       rst_n  
+    input  wire       rst_n,
+    input  wire [7:0] ui_in
 );
 
     // Internal signals
     wire sum1, c1, c2;
-    wire _unused = &{clk,ena,rst_n};
+    wire _unused = &{clk,ena,rst_n,ui_in,1'b0};
     // Sum calculation
     xor (sum1, a, b);
     xor (sum, sum1, cin);
