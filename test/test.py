f149f6bfef42d68275668dcebe8b1ecaec94f348
Yoda1999 <67253699+Yoda1999@users.noreply.github.com>
2024-12-26 13:08:10 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,11 +9,10 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
-
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-
+    
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
@@ -22,19 +21,30 @@ async def test_project(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-
+    
     dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    
+    # TEST 1: 0 + 0 + 0 = 0, carry = 0
+    dut.ui_in.value = 0b00000000  # a=0, b=0, cin=0
     await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    assert dut.uo_out.value == 0b00000000
+    
+    # TEST 2: 1 + 0 + 0 = 1, carry = 0
+    dut.ui_in.value = 0b00000001  # a=1, b=0, cin=0
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00000001
+    
+    # TEST 3: 1 + 1 + 0 = 0, carry = 1
+    dut.ui_in.value = 0b00000011  # a=1, b=1, cin=0
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00000010
+    
+    # TEST 4: 1 + 1 + 1 = 1, carry = 1
+    dut.ui_in.value = 0b00000111  # a=1, b=1, cin=1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00000011
+    
+    # TEST 5: 0 + 1 + 1 = 0, carry = 1
+    dut.ui_in.value = 0b00000110  # a=0, b=1, cin=1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00000010
